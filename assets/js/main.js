4ac2bf225d29e6d44499ed20ba85aefeb109682e
Tad <tad@spotco.us>
2017-06-11 21:46:52 -0400

    Remove loading screen

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,16 +11,6 @@
 		var	$window = $(window),
 			$body = $('body');
 
-		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
-
-			$window.on('load', function() {
-				window.setTimeout(function() {
-					$body.removeClass('is-loading');
-				}, 0);
-			});
-
-
 		// Fix: Placeholder polyfill.
 			$('form').placeholder();
 
