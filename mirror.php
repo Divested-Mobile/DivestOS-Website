d4a403fbf8040980c332f3403815f933ad047d52
Tad <tad@spotco.us>
2017-10-05 19:18:31 -0400

    Revert "More fixes"
    This reverts commit 8322fb3f1d1e56596e46bc247868072ecc41a196.

--- mirror.php
+++ mirror.php
@@ -8,7 +8,7 @@ $numMirrors = 0;
 $file = noHTML($_GET["f"]);
 $file = str_replace("&period;", ".", $file);
 $file = str_replace("&sol;", "/", $file);
-if(!is_null($file) && strlen($file) > 0 && substr_count($file, '.') == 2 && substr_count($file, '..') == 0 && substr_count($file, '/') <= 2) {
+if(!is_null($file) && substr_count($file, '.') == 2 && substr_count($file, '..') == 0 && substr_count($file, '/') <= 2) {
 	header('Location: ' . getMirror() . $file);
 } else {
 	print("Invalid request");
--- updater.php
+++ updater.php
@@ -4,7 +4,7 @@
 error_reporting(E_ERROR | E_PARSE);
 
 $device = strtolower(noHTML($_GET["device"]));
-if(!is_null($device) && strlen($device) > 0 && substr_count($device, '.') == 0 && substr_count($device, '/') == 0) {
+if(!is_null($device) && substr_count($device, '.') == 0 && substr_count($device, '/') == 0) {
 	$rootdir = "devices/" . $device;
 	$rootdirInc = $rootdir . "/incremental/";
 	if(is_dir($rootdir)) {
