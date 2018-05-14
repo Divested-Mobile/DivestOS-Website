a70a9938e811f5928c3f9eb75d473fbcb87e273a
Tad <tad@spotco.us>
2018-05-13 22:17:19 -0400

    SRI hashes

--- assets/js/load.js
+++ assets/js/load.js
@@ -1,7 +1,10 @@
-function loadExternalJS(url, callAfterLoad) {
+function loadExternalJS(url, callAfterLoad, integrity) {
 	var tmpJS = document.createElement('script');
 	tmpJS.type = "text/javascript";
 	tmpJS.src = url;
+	if(integrity !== undefined) {
+		tmpJS.integrity = integrity;
+	}
 	tmpJS.onload = callAfterLoad;
 	document.body.appendChild(tmpJS);
 }
