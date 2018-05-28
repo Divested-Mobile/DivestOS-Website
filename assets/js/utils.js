be621d853b2fb077a54320b007221adfbf71ac7e
Tad <tad@spotco.us>
2018-05-28 15:15:08 -0400

    Refractor and add dart theme toggle

---
+++ assets/js/utils.js
@@ -0,0 +1,34 @@
+function loadExternalJS(url, callAfterLoad, integrity) {
+	var tmpJS = document.createElement('script');
+	tmpJS.type = "text/javascript";
+	tmpJS.src = url;
+	if(integrity !== undefined) {
+		tmpJS.integrity = integrity;
+	}
+	tmpJS.onload = callAfterLoad;
+	document.body.appendChild(tmpJS);
+}
+
+//CREDIT: https://www.w3schools.com/js/js_cookies.asp
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+//CREDIT: https://www.w3schools.com/js/js_cookies.asp
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
