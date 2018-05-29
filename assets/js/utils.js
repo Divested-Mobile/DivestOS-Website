4b323da897bf87f42a920a4fad519793540b44a7
Tad <tad@spotco.us>
2018-05-29 02:01:53 -0400

    Add a delete cookies button to the privacy policy page

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -30,5 +30,16 @@ function setCookie(cname, cvalue, exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
     var expires = "expires="+d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";SameSite=Strict;path=/";
+}
+
+//CREDIT: https://stackoverflow.com/a/179514
+function deleteAllCookies() {
+    var cookies = document.cookie.split(";");
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        var eqPos = cookie.indexOf("=");
+        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    }
 }
