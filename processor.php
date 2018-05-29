d01db362943afd22b0626b2613aae83b2a759bfd
Tad <tad@spotco.us>
2018-05-29 01:32:24 -0400

    Add information about cookies to privacy policy

--- processor.php
+++ processor.php
@@ -3,22 +3,22 @@
 error_reporting(E_ERROR | E_PARSE);
 session_start();
 
-require_once('stripe/init.php');
-
-\Stripe\Stripe::setApiKey("pk_test_6pRNASCoBOKtIshFeQd4XMUh");
-
 $token = noHTML($_POST['stripeToken']);
 $ct = noHTML($_POST["ct"]);
 
 if($token != '' && $_SESSION['csrfToken'] == $ct) {
 	$_SESSION['csrfToken'] = bin2hex(random_bytes(64));
+
+	require_once('stripe/init.php');
+	\Stripe\Stripe::setApiKey("pk_test_6pRNASCoBOKtIshFeQd4XMUh");
+
 	try {
 		$charge = \Stripe\Charge::create(array(
 			"amount" => 1000,
 			"currency" => "usd",
 			"description" => "DivestOS Distro Download",
 			"source" => $token,
-	));
+			));
 	} catch(\Stripe\Error\Card $e) {
 		//Card Declined
 	} catch (\Stripe\Error\RateLimit $e) {
