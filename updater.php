e0f4cc3432af0914f554463fa84920e284a63fe5
Tad <tad@spotco.us>
2019-10-19 20:10:58 -0400

    updater: ignore in-progress uploads

--- updater.php
+++ updater.php
@@ -88,7 +88,7 @@ function getDeviceJson($rootdir, $rootdirInc, $base, $device, $inc) {
 }
 
 function getImageJson($rootdir, $base, $device, $image) {
-	if(!contains($image, "md5sum") && !contains($image, "sha512sum") && strlen($image) > 30) {
+	if(!contains($image, "md5sum") && !contains($image, "sha512sum") && strlen($image) > 30 && !startsWith($image, ".")) {
 		$imageSplit = explode("-", $image); //name-version-date-buildtype-device-[previnc].zip
 		unset($json);
 		if(startsWith(strtolower($imageSplit[4]), $device)) {
