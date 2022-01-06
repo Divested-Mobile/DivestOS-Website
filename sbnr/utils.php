433eff5459adc57dec631eeae6dbbda883ab62f6
Tad <tad@spotco.us>
2022-01-05 19:26:39 -0500

    humanTiming()
    Signed-off-by: Tad <tad@spotco.us>

--- sbnr/utils.php
+++ sbnr/utils.php
@@ -162,4 +162,25 @@ function minifyWhitespace($buffer) {
 	return $buffer;
 }
 
+//Credit (CC BY-SA 3.0): https://stackoverflow.com/a/2916189
+function humanTiming($time) {
+	$time = time() - $time; // to get the time since that moment
+	$time = ($time < 1) ? 1 : $time;
+	$tokens = array (
+		//31536000 => 'year',
+		//2592000 => 'month',
+		//604800 => 'week',
+		86400 => 'day',
+		3600 => 'hour',
+		60 => 'minute',
+		1 => 'second'
+	);
+
+	foreach ($tokens as $unit => $text) {
+		if ($time < $unit) continue;
+		$numberOfUnits = floor($time / $unit);
+		return $numberOfUnits . ' ' . $text . (($numberOfUnits>1) ? 's' : '');
+	}
+}
+
 ?>
