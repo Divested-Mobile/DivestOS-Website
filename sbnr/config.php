e3949f3c646067225dfb44b2cbc8f6ab43b4480a
Tad <tad@spotco.us>
2022-09-25 12:20:19 -0400

    Tweaks
    Signed-off-by: Tad <tad@spotco.us>

--- sbnr/config.php
+++ sbnr/config.php
@@ -33,7 +33,7 @@ $SBNR_SHADOW_LANDSCAPE = "DivestOS";
 //Security
 $SBNR_CAPTCHA_WALL = false;
 $SBNR_CAPTCHA_WALL_JS_REQUIRED = false;
-$SBNR_CAPTCHA_WALL_PAGES = array("devices");
+$SBNR_CAPTCHA_WALL_PAGES = array();
 $SBNR_SEC_HTTPS_ONLY = false; //Enable if using HTTPS
 $SBNR_SEC_CLIENT_HARDENING = false; //Disable if globally added by webserver
 $SBNR_SEC_FILE_UPLOADS = "Off";
