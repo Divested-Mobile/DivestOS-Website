62bded4bbe351ad680ede21a449c7fb8e05505f1
Tad <tad@spotco.us>
2023-02-06 18:07:52 -0500

    Break build related bits out of the website to another repo
    Signed-off-by: Tad <tad@spotco.us>

---
+++ download_devices.sh
@@ -0,0 +1,3 @@
+mkdir -p generated
+wget "https://mirror.divestos.org/generated/LineageOS-false.html" -O generated/LineageOS-false.html;
+wget "https://mirror.divestos.org/generated/LineageOS-true.html" -O generated/LineageOS-true.html;
--- gen_devices.sh
+++
@@ -1,3 +0,0 @@
-mkdir -p generated
-bwrap --dev-bind / / --ro-bind /dev/null /etc/ld.so.preload php gen_devices.php LineageOS false > generated/LineageOS-false.html
-bwrap --dev-bind / / --ro-bind /dev/null /etc/ld.so.preload php gen_devices.php LineageOS true  > generated/LineageOS-true.html
