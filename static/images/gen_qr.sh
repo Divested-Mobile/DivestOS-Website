a4636df0acea8580b84b505ba2fa9e79bd4d8c94
Tavi <tavi@divested.dev>
2024-01-05 13:37:02 -0500

    Fix the QR code
    Signed-off-by: Tavi <tavi@divested.dev>

--- static/images/gen_qr.sh
+++ static/images/gen_qr.sh
@@ -1,2 +1,2 @@
-echo -n "https://divestos.org/fdroid/official/?fingerprint=E4BE8D6ABFA4D9D4FEEF03CDDA7FF62A73FD64B75566F6DD4E5E577550BE8467" | qrencode --size=5 --level=M --foreground=FF5722 --background=ffffff -o divestos-official-fdroid-repo.png;
+echo -n "https://divestos.org/apks/official/fdroid/repo?fingerprint=E4BE8D6ABFA4D9D4FEEF03CDDA7FF62A73FD64B75566F6DD4E5E577550BE8467" | qrencode --size=5 --level=M --foreground=FF5722 --background=ffffff -o divestos-official-fdroid-repo.png;
 optipng -strip all divestos-official-fdroid-repo.png;
