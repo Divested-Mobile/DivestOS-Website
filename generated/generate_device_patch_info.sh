698064db3896af59aaaf3c26410cf00c2e66761c
Tavi <tavi@divested.dev>
2024-02-12 15:48:22 -0500

    2024-02 Changelog
    Signed-off-by: Tavi <tavi@divested.dev>

--- generated/generate_device_patch_info.sh
+++ generated/generate_device_patch_info.sh
@@ -45,7 +45,7 @@ getVendorPatchLevel() {
 		elif [ $1 == "LineageOS-20.0" ]; then
 			level="2023-09";
 		elif [ $1 == "LineageOS-21.0" ]; then
-			level="2024-01";
+			level="2024-02";
 		else
 			level="Unknown";
 		fi;
@@ -88,7 +88,7 @@ createTable() {
 	vASB=$(getVendorPatchLevel $version $devicePath)
 	#green: keep 4 months + 1 future
 	#yellow: previous year
-	if [[ $vASB == "2024-02"* ]] || [[ $vASB == "2024-01"* ]] || [[ $vASB == "2023-12"* ]] || [[ $vASB == "2023-11"* ]] || [[ $vASB == "2023-10"* ]]; then
+	if [[ $vASB == "2024-03"* ]] || [[ $vASB == "2024-02"* ]] || [[ $vASB == "2024-01"* ]] || [[ $vASB == "2023-12"* ]] || [[ $vASB == "2023-11"* ]]; then
 		vASBStyle="style=\"color:#4CAF50;\"";
 	elif [[ $vASB == "2023-"* ]]; then
 		vASBStyle="style=\"color:#FFC107;\"";
